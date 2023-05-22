1a5fbfee9505071e71a4bc8f526240bb5190248c
Cosmin Deaconu <cozzyd@gmail.com>
2023-05-22 01:12:29 -0500

    test multioutput

--- src/test-ret-writer.c
+++ src/test-ret-writer.c
@@ -9,7 +9,20 @@ static ret_radar_data_t radar;
 static cody_data_t cody[6]; 
 int main(int nargs, char ** args) 
 {
-  const char * outdir = nargs > 1 ? args[1] : "test"; 
+  int noutputs = nargs > 1 ? nargs - 1 : 1; 
+  const char * outdirs[noutputs]; 
+  if (nargs == 1) 
+  {
+    outdirs[0] = "test"; 
+  }
+  else
+  {
+    for (int i = 1; i < nargs; i++) 
+    {
+      outdirs[i-1] = args[i]; 
+    }
+  }
+
 
   struct timespec now;
   clock_gettime(CLOCK_REALTIME, &now); 
@@ -20,18 +33,18 @@ int main(int nargs, char ** args)
   radar.gps.weeknum = gps_weeks; 
   radar.gps.tow = tow * 1000 + now.tv_nsec / 1000000; 
   radar.gps.tow_f = now.tv_nsec % 1000000; 
-  printf("%d.%09d\n", now.tv_sec, now.tv_nsec);
+  printf("%ld.%09ld\n", now.tv_sec, now.tv_nsec);
   for (int i = 0; i < 6; i++) 
   {
     clock_gettime(CLOCK_REALTIME, &now); 
-    printf("%d.%09d\n", now.tv_sec, now.tv_nsec);
+    printf("%ld.%09ld\n", now.tv_sec, now.tv_nsec);
     cody[i].sci.gps_subtime.big = now.tv_nsec; 
     cody[i].sci.gps_time.big = now.tv_sec - 315964800 + 18; 
     usleep(1000); 
   }
 
   ret_full_event_t ev = {.radar = &radar, .cody = { &cody[0], &cody[1], &cody[2], &cody[3], &cody[4], &cody[5]}}; 
-  ret_writer_t *w = ret_writer_init(outdir); 
+  ret_writer_t *w = ret_writer_multi_init(noutputs, outdirs); 
   ret_writer_write_event(w,&ev); 
   ret_writer_destroy(w); 
 
