aadc501538eae2c1f8bc42da60ba42cab95fd5f4
Cosmin Deaconu <cozzyd@gmail.com>
2023-05-22 01:45:40 -0500

    further work on packetizer, fix makefile libs

--- Makefile
+++ Makefile
@@ -1,13 +1,13 @@
 BUILD_DIR=build
 CFLAGS=-Og -fPIC -Wall -Wextra -g -std=gnu11 -I./src -I/usr/include/libiberty 
 LDFLAGS=-shared  -lpthread
-LIBS=-lcurl -liberty -lm
+LIBS=-lcurl -liberty -lm -ltar -lmosquitto -lsystemd
 INCLUDES=src/radar.h
 
 .PHONY: radar  clean
 
 
-radar: $(BUILD_DIR)/libradar.so  $(BUILD_DIR)/radar-get  $(BUILD_DIR)/test-tarbuf $(BUILD_DIR)/test-cody-listener $(BUILD_DIR)/fake-cody $(BUILD_DIR)/test-ret-writer 
+radar: $(BUILD_DIR)/libradar.so  $(BUILD_DIR)/radar-get  $(BUILD_DIR)/test-tarbuf $(BUILD_DIR)/test-cody-listener $(BUILD_DIR)/fake-cody $(BUILD_DIR)/test-ret-writer  $(BUILD_DIR)/packetizer 
 
 clean: 
 	@echo Nuking $(BUILD_DIR) from orbit 
@@ -29,6 +29,6 @@ $(BUILD_DIR)/libradar.so: $(addprefix $(BUILD_DIR)/, $(RAD_OBJS))
 
 $(BUILD_DIR)/%: src/%.c
 	@echo Building $@
-	@cc -o $@ $(CFLAGS)  $^ $(LIBS) -L$(BUILD_DIR)/ -lradar -ltar -lmosquitto
+	@cc -o $@ $(CFLAGS)  $^ $(LIBS) -L$(BUILD_DIR)/ -lradar
 
 
--- src/packetizer.c
+++ src/packetizer.c
@@ -2,37 +2,64 @@
 #include "radar.h" 
 #include "ret-writer.h" 
 #include <stdlib.h>
+#include <signal.h>
+#include <math.h>
+#include <systemd/sd-daemon.h>
+#include <unistd.h>
+#include <time.h>
+#include <string.h>
 #include <stdio.h>
+#include <stdatomic.h> 
 #include <pthread.h>
 #include <systemd/sd-daemon.h> 
 
 
-struct ret_radar_t * radar; 
-struct cody_listener_t * cody_list; 
-struct ret_writer_t * writer; 
+ret_radar_t * radar; 
+cody_listener_t * cody_list; 
+ret_writer_t * writer; 
 
-size_t radar_buf_sz; 
-ret_radar_data_t * radar_buf; 
+#ifndef RADAR_BUF_SIZE
+#define RADAR_BUF_SIZE 128 
+#endif 
+
+#define MAX_OUT_DIRS 16
+
+
+//this holds the data
+ret_radar_data_t radar_buf[RADAR_BUF_SIZE]; 
+//this holds the read time. It also doubles as the free list (if time is  {0,0} then it's free)
+struct timespec radar_buf_read_times[RADAR_BUF_SIZE]; 
+volatile atomic_size_t radar_buf_free = RADAR_BUF_SIZE; 
+pthread_mutex_t radar_buf_lock = PTHREAD_MUTEX_INITIALIZER;  
+
+//store matches for each radar 
+struct 
+{
+  unsigned ncodies; 
+  const cody_data_t*  cody[6];
+} matches [RADAR_BUF_SIZE]; 
+
 
 const char * rfsoc_hostname = "192.168.97.30"; 
 int interrupt_gpio =23; 
 
 const char * ack_serial =  "/dev/serial/by-id/usb-Xilinx_JTAG+3Serial_68646-if02-port0"; 
 const char * gps_serial = "/dev/ttyAMA0"; 
-const char * mosquitto_host = "localhost" 
-const char * topicname = "/ret/SS%d/science_file"
+const char * mosquitto_host = "localhost"; 
+const char * topicname = "/ret/SS%d/science_file"; 
 int mosquitto_port = 1883; 
 uint8_t cody_mask = 0x3f; 
+uint8_t ncody; 
 
-const char* outdir = "/data"; 
+int noutdirs = 2; 
+const char* outdirs[MAX_OUT_DIRS] = { "/data0", "/data1", 0}; 
 
 static pthread_t the_radar_thread;;
-static pthread_t the_cody_thread;  
 static pthread_t the_collect_and_write_thread;  
 
 volatile int break_flag = 0; 
 
-int max_age = 30; // max age before giving up trying to write 
+double max_age = 30; // max age before giving up trying to write as a normal event
 double assoc_thresh = 1e-3; // 1 ms 
 
 void sighandler(int sig) 
@@ -41,13 +68,20 @@ void sighandler(int sig)
   break_flag = 1; 
 }
 
+double delta_time(const struct timespec * a, const struct timespec *b) 
+{
+  return (a->tv_sec - b->tv_sec) + 1e9*(a->tv_nsec - b->tv_nsec); 
+}
+
+struct timespec newest_radar; 
+struct timespec newest_cody; 
 
 
 void usage() 
 {
-  fprintf(stderr, "packetizer [-H RFSOC_HOSTNAME=%s] [-i rfsoc-interrupt-gpio = %d] [-a rfsoc_ack_serial = %s ]\n", hostname, interrupt_gpio, ack_serial);
-  fprintf(stderr, "           [-g rfsoc_gps_serial = %s] [-M mosquitto host = localhost] [-C CODY_MASK = 0x%x]\n", gps_serial, cody_mask); 
-  fprintf(stderr,"            [-p mosquitto-port= %d] [-o outdir=%s] [-t topicname = %s]\n", mosquitto_port, outdir,topicname); 
+  fprintf(stderr, "packetizer [-H RFSOC_HOSTNAME=%s] [-i rfsoc-interrupt-gpio = %d] [-a rfsoc_ack_serial = %s ]\n", rfsoc_hostname, interrupt_gpio, ack_serial);
+  fprintf(stderr, "           [-g rfsoc_gps_serial = %s] [-M mosquitto host = %s] [-C CODY_MASK = 0x%hhx]\n", gps_serial, mosquitto_host, cody_mask); 
+  fprintf(stderr,"            [-p mosquitto-port= %d] [-o0 outdir=%s] [-o1 outdir2 = %s ][-t topicname = %s]\n", mosquitto_port, outdirs[0], outdirs[1],topicname); 
 }
 
 
@@ -60,7 +94,7 @@ int parse(int nargs, char ** args)
 
     if (!strcmp(args[i],"-H"))
     {
-      hostname = args[++i]; 
+      rfsoc_hostname = args[++i]; 
     }
     else if (!strcmp(args[i],"-i"))
     {
@@ -80,15 +114,22 @@ int parse(int nargs, char ** args)
     }
     else if (!strcmp(args[i],"-C"))
     {
-      cody_mask = strtod(args[++i],0,0); 
+      cody_mask = strtol(args[++i],0,0); 
     }
     else if (!strcmp(args[i],"-p"))
     {
-      mosquitto_port = atoi(args[++i])
+      mosquitto_port = atoi(args[++i]);
     }
-    else if (!strcmp(args[i],"-o"))
+    else if (args[i] == strstr(args[i],"-o"))
     {
-      outdir = args[++i]; 
+      int ioutput = -1; 
+      sscanf(args[i], "-o%d", &ioutput); 
+      if (ioutput >=0 && ioutput < MAX_OUT_DIRS)
+      {
+        outdirs[ioutput] = args[++i]; 
+        if (ioutput+1 > noutdirs) noutdirs = ioutput+1; 
+        printf("Using output directory %d of %s\n", ioutput, outdirs[ioutput]); 
+      }
     }
     else if (!strcmp(args[i],"-t"))
     {
@@ -105,24 +146,160 @@ int parse(int nargs, char ** args)
 
 
 
-// radar thread grabs radar events, puts them in a the radar bufber
+// radar thread grabs radar events, puts them in a the radar buffer
 void * radar_thread(void*)  
 {
 
+  while(!break_flag) 
+  {
 
+    static ret_radar_data_t data;
+    if (!ret_radar_next_event(radar, &data))
+    {
+      
+      while(1) 
+      {
+
+        if (!radar_buf_free)
+        {
+          usleep(100e3); 
+          continue; 
+        }
+        
+        int parallel_universe = 0; 
+
+        //we are trying to add to the queue, so we need to make sure nothing is removed at the same time,
+        //so we lock it
+        pthread_mutex_lock(&radar_buf_lock); 
+        for (size_t i = 0; i < RADAR_BUF_SIZE; i++) 
+        {
+          if (radar_buf_read_times[i].tv_sec == 0 && radar_buf_read_times[i].tv_nsec == 0)
+          {
+            ret_radar_fill_time(&data.gps, &radar_buf_read_times[i]); 
+            if (delta_time(&radar_buf_read_times[i], &newest_radar) > 0)
+            {
+              newest_radar = radar_buf_read_times[i];
+            }
+            else
+            {
+              parallel_universe++; 
+            }
+
+            memcpy(&radar_buf[i], &data, sizeof(data)); 
+            radar_buf_free--; 
+          }
+        }
+        pthread_mutex_unlock(&radar_buf_lock); 
+
+        if (parallel_universe) 
+           fprintf(stderr, "WARNING: Radar events might be going back in time?\n"); 
+      }
+    }
+  }
+  return NULL; 
 }
 
-void * cody_thread(void*) 
-{
-
-
-}
 
 void * collect_and_write_thread(void*) 
 {
 
 
+  while (!break_flag) 
+  {
+    // check if any rfsoc events are ready, if so start a match... 
+    // with the same index as the buffer
+    
+    if (radar_buf_free < RADAR_BUF_SIZE)
+    {
+      //retrieve the ones that are filled.
+      static uint8_t filled_entries[RADAR_BUF_SIZE] = {0}; 
+      pthread_mutex_lock(&radar_buf_lock); 
+      for (size_t i = 0; i < RADAR_BUF_SIZE; i++)
+      {
+        filled_entries[i] = !!radar_buf_read_times[i].tv_sec; 
+      }
+      pthread_mutex_unlock(&radar_buf_lock); 
 
+      //now go through the codies in our mask 
+      //now loop over again
+
+      for (int i = 0; i < RADAR_BUF_SIZE; i++) 
+      {
+        if (!filled_entries[i]) continue; 
+
+        for (int icody = 0; icody<6; icody++) 
+        {
+          if ( (cody_mask & (1 << icody)) == 0) continue; 
+
+          //no match yet for this cody 
+          if ( !matches[i].cody[icody])
+          {
+
+            int nrdy = cody_listener_nrdy(cody_list, icody+1); //cody here is 1-indexed
+            if (!nrdy) continue;                                                                
+
+            
+            for (int irdy = 0; irdy < nrdy; irdy++) 
+            {
+
+              const cody_data_t * cody = cody_listener_get(cody_list, icody+1, irdy); 
+
+              struct timespec ts; 
+              cody_data_fill_time(cody,&ts); 
+
+              if (delta_time(&ts, &newest_cody) > 0) 
+              {
+                newest_cody = ts; 
+              }
+
+
+              if (fabs(delta_time(&radar_buf_read_times[i], &ts)) < assoc_thresh)
+              {
+                
+                 matches[i].cody[icody] = cody; 
+                 matches[i].ncodies++; 
+                 break; 
+              }
+
+              double age = delta_time(&newest_cody, &ts);
+              // check if we have passed max age on this cody 
+              if (age > max_age)
+              {
+                fprintf(stderr,"Found unmatched event that is %f seconds older than the newest CODY event. Writing it out to stag folder.\n", age); 
+                ret_writer_write_stag_cody(writer, cody, icody+1); 
+                cody_listener_release(cody_list,cody); 
+              }
+            }
+          }
+        }
+
+        //see if we have a complete match, or if we need to max_age...  
+        if (matches[i].ncodies == ncody)//huzzah! 
+        {
+          ret_full_event_t full = {
+            .radar = &radar_buf[i], 
+             .cody = { 
+               matches[i].cody[0],
+               matches[i].cody[1], 
+               matches[i].cody[2], 
+               matches[i].cody[3], 
+               matches[i].cody[4], 
+               matches[i].cody[5]
+             }
+          };
+
+          ret_writer_write_event(writer, &full); 
+
+          //now we can release everythig related to this event
+        }
+
+
+      }
+    }
+  }
+
+
+  return NULL; 
 }
 
 
@@ -135,6 +312,9 @@ int main(int nargs, char ** args)
   {
     usage(); 
     return 1; 
+  }
+
+  ncody = __builtin_popcount(cody_mask); 
 
   signal(SIGINT, sighandler); 
 
@@ -146,22 +326,23 @@ int main(int nargs, char ** args)
     return 1; 
   }
 
-  cody_list = cody_listener_init(mosquitto_host, mosquitto_port, topic_string, cody_mask, 64,0); 
+  cody_list = cody_listener_init(mosquitto_host, mosquitto_port, topicname, cody_mask, 64,0); 
   if (!cody_list) 
   {
-    fprintf(stderr, "Could not open cody listener at %s:%d\n", mosquitt_host, mosquitto_port); 
+    fprintf(stderr, "Could not open cody listener at %s:%d\n", mosquitto_host, mosquitto_port); 
     return 1; 
   }
 
-  writer = ret_writer_init(outdir); 
+  writer = ret_writer_multi_init(noutdirs, outdirs); 
   if (!writer) 
   {
-    fprintf(stderr,"Could int initialize writer with out dir %s\n", outdir); 
+    fprintf(stderr,"Could int initialize writer\n"); 
     return 1; 
   }
 
+
+
   pthread_create(&the_radar_thread, NULL, radar_thread, NULL); 
-  pthread_create(&the_cody_thread, NULL, cody_thread, NULL); 
   pthread_create(&the_collect_and_write_thread, NULL, collect_and_write_thread, NULL); 
   
   while (!break_flag)
@@ -174,7 +355,6 @@ int main(int nargs, char ** args)
 
   pthread_join(the_radar_thread, 0);
   ret_radar_close(radar); 
-  pthread_join(the_cody_thread, 0);
   cody_listener_finish(cody_list); 
   pthread_join(the_collect_and_write_thread, 0);
   ret_writer_destroy(writer); 
