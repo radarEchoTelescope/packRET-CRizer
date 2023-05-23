b48f364609c2e7f4fff51fe35abb37d7e21c40b8
Cosmin Deaconu <cozzyd@gmail.com>
2023-05-23 00:23:25 -0500

    missing file

---
+++ src/radar-hk.c
@@ -0,0 +1,39 @@
+
+#include "radar.h" 
+#include <stdio.h> 
+#include <time.h> 
+
+
+const char * serial = "/dev/ttyAMA1"; 
+
+int main(int nargs, char ** args)
+{
+
+  if (nargs > 1) serial = args[1]; 
+
+
+  ret_radar_hk_t * hk = ret_radar_hk_open(serial); 
+  if (!hk) 
+  {
+    printf("Could not open %s\n", serial); 
+    return 1; 
+  }
+
+  ret_radar_hk_data_t data; 
+
+  ret_radar_hk_fill(hk,&data); 
+  time_t now = time(0); 
+  struct tm * gmt = gmtime(&now); 
+  printf("==========At %s=============\n", asctime(gmt)); 
+  printf("Board Temperature: %f\n", data.board_temp); 
+  printf("Air Temperature: %f\n", data.air_temp); 
+  printf("Vin: %f\n", data.vin); 
+  
+
+  ret_radar_hk_close(hk); 
+
+  return 0; 
+
+
+
+}
