9ff1236c9d74da87aeaa30b00574d1ee55778573
Cosmin Deaconu <cozzyd@gmail.com>
2023-05-24 01:52:02 -0500

    first attempt at threaded radar-get

--- src/radar-get.c
+++ src/radar-get.c
@@ -5,10 +5,12 @@
 #include <time.h>
 #include <string.h> 
 #include "ret-writer.h" 
+#include <pthread.h> 
 #include <stdlib.h> 
+#include <stdatomic.h> 
 
+#define RADAR_BUF_SIZE 16 
 
-static ret_radar_data_t d;
 
 const char * hostname = "192.168.97.30"; 
 int interrupt_gpio = 23;
@@ -21,7 +23,8 @@ int quiet = 0;
 
 int noutput_dirs = 0;
 const char * output_dir[16] = {0};
-
+ret_radar_t * radar;  
+ret_writer_t * writer;  
 volatile int break_flag = 0; 
 
 void sighandler(int sig) 
@@ -36,6 +39,83 @@ void usage()
   fprintf(stderr, "radar-get [-h HOSTNAME=%s] [-i interrupt-gpio = %d] [-a ack_serial = %s ] [-g gps_serial = %s] [-N number = %d] [-o output_dir =(none)] [-o additional_output_dir=(none)] [-z] [-V] [-q]\n",
       hostname, interrupt_gpio, ack_serial, gps_serial, N); 
   exit(1); 
+}
+
+
+
+
+volatile atomic_size_t nreceived = 0;
+volatile atomic_size_t nwritten = 0;
+ret_radar_data_t radar_buf[RADAR_BUF_SIZE]; 
+
+
+
+pthread_t the_acq_thread; 
+pthread_t the_write_thread; 
+
+void * acq_thread(void * ignored) 
+{
+  (void) ignored; 
+  while (!break_flag) 
+  {
+    //is buffer full? 
+
+    if (atomic_load_explicit(&nreceived,memory_order_acquire) - atomic_load_explicit(&nwritten,memory_order_acquire) == RADAR_BUF_SIZE)
+    {
+      fprintf(stderr,"WARNING: Buffer Full?\n"); 
+      usleep(10e4); //10 ms
+      continue; 
+    }
+
+    if (!ret_radar_next_event(radar, &radar_buf[atomic_load_explicit(&nreceived, memory_order_acquire) % RADAR_BUF_SIZE])) 
+    {
+      atomic_fetch_add(&nreceived,1); 
+    }
+  }
+
+  return NULL;
+}
+
+void * write_thread(void* ignored)
+{
+  (void) ignored; 
+
+  while (!break_flag) 
+  {
+    // nothing to write 
+    if (atomic_load_explicit(&nreceived, memory_order_acquire) == atomic_load_explicit(&nwritten, memory_order_acquire) )
+    {
+      usleep(10e4); //10 ms
+      continue; 
+    }
+
+    size_t idx = atomic_load_explicit(&nwritten, memory_order_acquire); 
+    ret_radar_data_t *d = &radar_buf[idx % RADAR_BUF_SIZE]; 
+
+    if (noutput_dirs > 0) 
+    {
+        ret_writer_write_radar(writer, d); 
+    }
+ 
+    if (!quiet) 
+    {
+        printf("  {\n"); 
+        printf("    \"i\":%d,\n", (int) nwritten); 
+        printf("    \"radar\" :"); 
+        ret_radar_rfsoc_dump(stdout,&d->rfsoc, 6); 
+        printf("    , \"gps_tm\" :\n"); 
+        ret_radar_gps_tm_dump(stdout,&d->gps, 6); 
+        printf("   }\n"); 
+    }
+    else
+    {
+        printf(" Event %d\n", (int) nwritten); 
+    }
+
+    int nevents = atomic_fetch_add(&nwritten,1); 
+    if (N > 0 && nevents >= N) break_flag = 1; 
+  }
+  return NULL;
 }
 
 int main(int nargs, char ** args)
@@ -61,9 +141,6 @@ int main(int nargs, char ** args)
       quiet = 1; 
       continue; 
     }
-
-
-
 
     if (i == nargs -1) usage(); 
 
@@ -111,54 +188,31 @@ int main(int nargs, char ** args)
 
   signal(SIGINT, sighandler); 
 
-  ret_radar_t * radar = ret_radar_open(hostname, interrupt_gpio, ack_serial, gps_serial);
+  radar = ret_radar_open(hostname, interrupt_gpio, ack_serial, gps_serial);
   ret_radar_set_verbose(radar,verbose); 
   ret_radar_set_timeout(radar,1); 
 
-  int nevents = 0; 
 
-  ret_writer_t * w = 0; 
 
   if (noutput_dirs > 0) 
   {
-     w = ret_writer_multi_init(noutput_dirs, output_dir, compress);  
+     writer = ret_writer_multi_init(noutput_dirs, output_dir, compress);  
   }
 
   if (!quiet) 
     printf("{\n  \"events\" = [ "); 
-  while(1) 
-  {
-    if (N > 0 && nevents > N) break; 
-    if (!ret_radar_next_event(radar, &d)) 
-    {
-      if (!quiet) 
-      {
-        printf("  {\n"); 
-        printf("    \"i\":%d,\n", nevents); 
-        printf("    \"radar\" :"); 
-        ret_radar_rfsoc_dump(stdout,&d.rfsoc, 6); 
-        printf("    , \"gps_tm\" :\n"); 
-        ret_radar_gps_tm_dump(stdout,&d.gps, 6); 
-        printf("   }\n"); 
-      }
-      else
-      {
-        printf(" Event %d\n", nevents); 
-      }
-      fflush(stdout); 
 
-      nevents++; 
-      if (noutput_dirs > 0) 
-      {
-        ret_writer_write_radar(w, &d); 
-      }
-      if (break_flag) break; 
-    }
-    if (break_flag) break; 
-  }
+  pthread_create(&the_acq_thread, NULL,acq_thread,NULL); 
+  pthread_create(&the_write_thread, NULL,write_thread,NULL); 
 
-   if (!quiet) printf("  ]\n}\n"); 
+  pthread_join(the_write_thread,0);
+
+  if (!quiet)
+    printf("  ]\n}\n"); 
+  else 
+    printf("Exiting\n"); 
   ret_radar_close(radar); 
+  ret_writer_destroy(writer); 
   return 0; 
 }
 
