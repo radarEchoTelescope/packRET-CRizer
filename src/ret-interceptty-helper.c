cfe97e74db872960fc95278ea6e792e61419bd33
Cosmin Deaconu <cozzyd@gmail.com>
2023-06-11 11:26:32 -0500

    try to reopen fifo if lost?

--- src/ret-interceptty-helper.c
+++ src/ret-interceptty-helper.c
@@ -15,46 +15,56 @@ int main(int nargs, char ** args)
   int match_len = strlen(match); 
 
   printf("Match string is \"%s\"\n", match); 
-  printf("Trying to open %s, this might block if it's not open!\n", fifo_name); 
-  int fd = open(fifo_name, O_WRONLY); 
 
-  if (fd > 0) 
+  int fd = 0;
+  while(1) 
   {
-    printf("Successfully opened %s\n", fifo_name); 
-  }
-  else
-  {
-    printf("open(%s) returned bad fd\n",fifo_name); 
-    return 1; 
-  }
+    printf("Trying to open %s, this might block if it's not open!\n", fifo_name); 
+    fd = open(fifo_name, O_WRONLY); 
 
-  char * line = NULL;
-  size_t line_size = 0; 
-
-  int nmatch = 0; 
-  while (getline(&line, &line_size, stdin) )
-  {
-    if (line[0]!='>' || line_size < 10) continue; 
-
-    // format is 
-    // > \t0xXX (C)"
-    // 012 3456789
-    // so we want 9th character is a match and 10th character = ) 
-
-    if (line[9] == match[nmatch] && line[10]==')')
+    if (fd > 0) 
     {
-      nmatch++; 
+      printf("Successfully opened %s\n", fifo_name); 
     }
     else
     {
-      nmatch = 0; 
+      printf("open(%s) returned bad fd\n",fifo_name); 
+      return 1; 
     }
 
-    //great success
-    if (nmatch == match_len) 
+    char * line = NULL;
+    size_t line_size = 0; 
+
+    int nmatch = 0; 
+    while (getline(&line, &line_size, stdin) )
     {
-      printf("Matched string!\n"); 
-      write(fd,"1",1); 
+      if (line[0]!='>' || line_size < 10) continue; 
+
+      // format is 
+      // > \t0xXX (C)"
+      // 012 3456789
+      // so we want 9th character is a match and 10th character = ) 
+
+      if (line[9] == match[nmatch] && line[10]==')')
+      {
+        nmatch++; 
+      }
+      else
+      {
+        nmatch = 0; 
+      }
+
+      //great success
+      if (nmatch == match_len) 
+      {
+        printf("Matched string!\n"); 
+        if (1!=write(fd,"1",1))
+        {
+          fprintf(stderr,"Failed to write, is there nothing listening? Will try to reopen\n"); 
+          close(fd); 
+          break; 
+        }
+      }
     }
   }
 
